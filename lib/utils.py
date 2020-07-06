7c6ec80f673b7d3b3ae74e62899d264b6a1b8ef2
Pranjali <pj10kumbhar@gmail.com>
2020-07-06 18:55:13 +0530

    Initialized commit

---
+++ lib/utils.py
@@ -0,0 +1,87 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
+def apply_canny(img, sigma=0.33):
+    v = np.median(img)
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    return cv2.Canny(img, lower, upper)
+
+def plt_show(*images):
+    count = len(images)
+    nRow = np.ceil(count / 3.)
+    for i in range(count):
+        plt.subplot(nRow, 3, i + 1)
+        if len(images[i][0].shape) == 2:
+            plt.imshow(images[i][0], cmap='gray')
+        else:
+            plt.imshow(images[i][0])
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(images[i][1])
+    plt.show()
+
+
+
+
+def sobel(img):
+    # Open the image
+    img = np.array(Image.open('rPICT0004.jpg')).astype(np.uint8)
+
+
+
+    # Apply gray scale
+    gray_img = np.round(0.299 * img[:, :, 0] +
+                    0.587 * img[:, :, 1] +
+                    0.114 * img[:, :, 2]).astype(np.uint8)
+
+    # Sobel Operator
+    h, w = gray_img.shape
+
+    # define filters
+    horizontal = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])  # s2
+    vertical = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])  # s1
+
+    # define images with 0s
+    newhorizontalImage = np.zeros((h, w))
+    newverticalImage = np.zeros((h, w))
+    newgradientImage = np.zeros((h, w))
+
+    # offset by 1
+    for i in range(1, h - 1):
+        for j in range(1, w - 1):
+            horizontalGrad = (horizontal[0, 0] * gray_img[i - 1, j - 1]) + \
+                         (horizontal[0, 1] * gray_img[i - 1, j]) + \
+                         (horizontal[0, 2] * gray_img[i - 1, j + 1]) + \
+                         (horizontal[1, 0] * gray_img[i, j - 1]) + \
+                         (horizontal[1, 1] * gray_img[i, j]) + \
+                         (horizontal[1, 2] * gray_img[i, j + 1]) + \
+                         (horizontal[2, 0] * gray_img[i + 1, j - 1]) + \
+                         (horizontal[2, 1] * gray_img[i + 1, j]) + \
+                         (horizontal[2, 2] * gray_img[i + 1, j + 1])
+
+            newhorizontalImage[i - 1, j - 1] = abs(horizontalGrad)
+
+            verticalGrad = (vertical[0, 0] * gray_img[i - 1, j - 1]) + \
+                       (vertical[0, 1] * gray_img[i - 1, j]) + \
+                       (vertical[0, 2] * gray_img[i - 1, j + 1]) + \
+                       (vertical[1, 0] * gray_img[i, j - 1]) + \
+                       (vertical[1, 1] * gray_img[i, j]) + \
+                       (vertical[1, 2] * gray_img[i, j + 1]) + \
+                       (vertical[2, 0] * gray_img[i + 1, j - 1]) + \
+                       (vertical[2, 1] * gray_img[i + 1, j]) + \
+                       (vertical[2, 2] * gray_img[i + 1, j + 1])
+
+            newverticalImage[i - 1, j - 1] = abs(verticalGrad)
+
+        # Edge Magnitude
+        mag = np.sqrt(pow(horizontalGrad, 2.0) + pow(verticalGrad, 2.0))
+        newgradientImage[i - 1, j - 1] = mag
+        
+      
+    return newgradientImage
+
+
